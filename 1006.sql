78e6271a3034f53cfd88da4f6445c10bab83a0d8
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-17 12:07:53 +0600

    Search column from Table

---
+++ 1006.sql
@@ -0,0 +1,15 @@
+/* search column from table: (must use this command inside the database,like database name=University & table name is teacher) */
+/* SELECT column_name;
+   FROM table_name */
+
+/*Way-1 for search data from a table*/
+
+SELECT ID 
+FROM teacher;
+
+/*Way-2 for search data from a table*/
+
+SELECT ID,Department,Gender
+FROM teacher;
+
+
